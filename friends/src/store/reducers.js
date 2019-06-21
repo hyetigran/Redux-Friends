97ca67512ba2329550eaab3c10be4ac2c99e0c89
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-06-21 08:45:46 +0400

    added reducer

---
+++ friends/src/store/reducers.js
@@ -0,0 +1,38 @@
+import * as actionTypes from '../actions';
+
+const initialState = {
+	friends: [],
+	loading: false,
+	error: null
+};
+
+const updateObject = (oldObject, updatedProperties) => {
+	return {
+		...oldObject,
+		...updatedProperties
+	};
+};
+
+const fetchFriendStart = (state, action) => {
+	return updateObject(state, { loading: true });
+};
+
+const fetchFriendSuccess = (state, action) => {
+	return updateObject(state, { friends: action.friends, loading: false });
+};
+const fetchFriendFail = (state, action) => {
+	return updateObject(state, { loading: false });
+};
+
+export const friendsReducer = (state = initialState, action) => {
+	switch (action.type) {
+		case actionTypes.FETCH_FRIEND_START:
+			return fetchFriendStart(state, action);
+		case actionTypes.FETCH_FRIEND_SUCCESS:
+			return fetchFriendSuccess(state, action);
+		case actionTypes.FETCH_FRIEND_FAIL:
+			return fetchFriendFail(state, action);
+		default:
+			return state;
+	}
+};
